27a708007e2bcfcfaf76c7813ce5b38a9530b47c
dustin-kang <dongwoomail@icloud.com>
2024-01-11 09:14:08 +0900

    #4 django app

---
+++ houses/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class House(models.Model):
+    
+    """
+    Model Deifinition for Houses
+    """
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField(verbose_name = "Price?", help_text="Positive number only")
+    decription = models.TextField()
+    address = models.CharField(max_length=140)
+    pet_allowed = models.BooleanField(default=True, help_text="Does this house allows pets?", verbose_name="Pets Allowed?")
+
+    def __str__(self):
+        return self.name
