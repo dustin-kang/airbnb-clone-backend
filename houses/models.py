29e839e626b90881fc6739fca647c3ce614d0cbe
Dongwoo Kang <dongwoomail@icloud.com>
2024-01-11 12:15:21 +0900

    #5 Django Users App

--- houses/models.py
+++ houses/models.py
@@ -10,7 +10,8 @@ class House(models.Model):
     price_per_night = models.PositiveIntegerField(verbose_name = "Price?", help_text="Positive number only")
     decription = models.TextField()
     address = models.CharField(max_length=140)
-    pet_allowed = models.BooleanField(default=True, help_text="Does this house allows pets?", verbose_name="Pets Allowed?")
+    pets_allowed = models.BooleanField(default=True, help_text="Does this house allows pets?", verbose_name="Pets Allowed?")
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
