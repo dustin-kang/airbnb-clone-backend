0585fa146ce3ffd0d2b3ad375fd8ae2c61cfb6cc
Dongwoo Kang <dongwoomail@icloud.com>
2024-01-18 19:29:48 +0900

    #10 ModelViewSet ModelView

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,18 +1,8 @@
 from rest_framework import serializers
 from .models import Category
 
-class CategorySerializer(serializers.Serializer):
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "kind", "created_at"]
 
-    pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.ChoiceField(choices=Category.CategoryKindChoices.choices,) # ChoiceField
-    created_at = serializers.DateTimeField()
-
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data) ## **는 딕셔너리를 가져오게 된다.
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.kind = validated_data.get("name", instance.kind)
-        instance.save()
-        return instance
--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,6 +10,11 @@ from .serializers import CategorySerializer
 
 # Create your views here.
 
+
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    
 class Categories(APIView):
     
     def get(self, request):
