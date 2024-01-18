0f2c9f60f4b46e235d23d0711a14491a9e99fe75
Dongwoo Kang <dongwoomail@icloud.com>
2024-01-19 08:10:45 +0900

    #11 Amenities, Detail

--- rooms/views.py
+++ rooms/views.py
@@ -1,35 +1,55 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Room
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rooms.models import Amenity, Room
+from rooms.serializers import AmenitySerializer
 # Create your views here.
 
 
-def see_all_rooms(request):
-    rooms = Room.objects.all()
-    return render(
-        request,
-        "all_rooms.html",
-        {
-            "rooms": rooms,
-            "title": "Hello! this title comes from django!",
-        },
-    )
+class Amenities(APIView):
 
-def see_one_room(request, room_pk):
-    try:
-        room = Room.objects.get(pk=room_pk)
-        return render(
-            request,
-            "room_detail.html",
-            {
-                "room": room,
-            },
-        )
-    except Room.DoesNotExist:
-        return render(
-            request,
-            "room_detail.html",
-            {
-                "not_found": True,
-            },
-        )
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(all_amenities, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializers = AmenitySerializer(data = request.data)
+        if serializers.is_valid():
+            amenity = serializers.save()
+            return Response(
+                AmenitySerializer(amenity).data,
+            )
+        else:
+            return Response(serializers.errors)
+
+class AmenityDetail(APIView):
+
+    def get_object(self, pk):
+        # 데이터를 가져오는 함수
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(amenity)
+        return Response(serializer.data)
+
+    def put(self, request, pk): # url에 Pk 변수를 사용하므로 파라미터로 넣어야함.
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(amenity, data=request.data, partial=True)
+        if serializer.is_valid():
+            updated_amenity = serializer.save()
+            return Response(
+                AmenitySerializer(updated_amenity).data,
+            )
+        else:
+            return Response(serializer.errors)            
+    
+    def delete(self, request, pk):
+        amenity = self.get_object(pk)
+        amenity.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
