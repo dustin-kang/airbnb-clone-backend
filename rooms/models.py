ec4c934727970f208ec43fa943e1a13d8fc15b5a
Dongwoo Kang <dongwoomail@icloud.com>
2024-01-29 21:10:56 +0900

    #11 `SerializerMethodField`
    - 시리얼라이저에서 본인이 만든 모델 메서드를 호출하는 방법, 조회수등 활용

--- rooms/models.py
+++ rooms/models.py
@@ -37,7 +37,7 @@ class Room(models.Model):
     def rating(room):
         count =  room.reviews.count()
         if count == 0:
-            return "No Reviews"
+            return 0
         else:
             total_rating = 0
             for review in room.reviews.all().values("rating"):
--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Amenity, Room
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
@@ -14,12 +14,19 @@ class RoomDetailSerializer(ModelSerializer):
     owner = TinyUserSerializer(read_only=True) # 사용자에게 owner 입력에 대한 권한을 주면 안됨. -> read_only=True
     amenities = AmenitySerializer(read_only=True, many=True)
     category = CategorySerializer(read_only=True)
+    rating = SerializerMethodField()
 
     class Meta:
         model = Room
         fields = "__all__"
 
+    def get_rating(self, room): # get_계산하려는 속성
+         return room.rating()
+
 class RoomListSerializer(ModelSerializer):
-        class Meta:
-            model = Room
-            fields = ["pk", "name", "country", "city", "price"]
+    class Meta:
+        model = Room
+        fields = ["pk", "name", "country", "city", "price", "rating",]
+
+    def get_rating(self, room):
+        return room.rating()
