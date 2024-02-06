6ce448421c7d0fee0c76d82b4b3dc7721a3b1a47
Dongwoo Kang <dongwoomail@icloud.com>
2024-02-06 16:39:44 +0900

    #11 특정 필드 Valid 커스터마이징

--- rooms/views.py
+++ rooms/views.py
@@ -12,7 +12,7 @@ from rooms.serializers import AmenitySerializer, RoomListSerializer, RoomDetailS
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 
 # Create your views here.
 
@@ -259,3 +259,11 @@ class RoomBookings(APIView):
         bookings = Booking.objects.filter(room=room, kind=Booking.BookingKindChoices.ROOM, check_in__gt=now,)
         serializer = PublicBookingSerializer(bookings, many=True)
         return Response(serializer.data)
+    
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"ok": True})
+        else:
+            return Response(serializer.errors)        
