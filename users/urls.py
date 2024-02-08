ac5a94b010bd5d2c3bb3d6c1d8fd2e8c618f72ce
Dongwoo Kang <dongwoomail@icloud.com>
2024-02-08 16:00:36 +0900

    #15 JWT encoded - `poetry add pyjwt` -  Auth Token과 다르게 데이터베이스에 저장하지 않고 토큰을 열어 유저 정보를 찾는 방식

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from users import views
 
 urlpatterns = [
     path("token-login", obtain_auth_token), # 해당 view에 username과 password를 보내면 토큰을 받는다.
+    path("jwt-login", views.JWTLogIn.as_view())    
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import jwt
+from django.conf import settings
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.exceptions import ParseError, NotFound
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class JWTLogin(APIView):
+    def post(self, request):
+        username =  request.data.get("usernmae")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(
+            request,
+            username = username,
+            password = password
+        )
+        if user:
+            # 토큰이 안전한 이유 : 암호화 X, 누가 준 토큰인지 알 수 있기 때문
+            token = jwt.encode(
+                {"pk": user.pk},
+                settings.SECRET_KEY,
+                algorithm="HS256"
+            )
+            return Response({"token": token})
+        else:
+            return Response({"error": "wrong password"})
+        
